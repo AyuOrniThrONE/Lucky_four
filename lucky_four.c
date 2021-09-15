d9859d6e85c243c9e01e46d918f33179fdf87a1b
AyuOrniThrONE <ayushprakashthaware@gmail.com>
2021-09-15 18:21:20 +0530

    added

--- lucky_four.c
+++ lucky_four.c
@@ -1,29 +1,40 @@
 #include <stdio.h>
 int main()
 {
-    int T;
+    int T,occ = 0;
     scanf("%d", &T);
     for (int i = 0; i < T; i++)
     {
-        int n, remain = 0, occ = 0;
+        int n, remain = 0; 
         scanf("%d", &n);
     label:
     label1:
+    //labe3l:
         remain += n % 10;
         n /= 10;
+        // while (n==0){
+        //     if (remain == 0)
+        //     {
+        //         occ++;
+        //         remain=0;
+        //         goto labe3l;
+        //     }
+        // }
         while (n >= 4)
         {
             if (remain == 4)
             {
                 occ++;
+                remain=0;
+                goto label;
             }
+            
             else
             {
+                remain=0;
                 goto label1;
             }
         }
-        goto label;
-
         printf("%d\n", occ);
     }
     return 0;
